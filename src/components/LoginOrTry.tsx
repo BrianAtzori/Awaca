0d055c720b9c08af32283cb20756dd9c1f2c7cff
BrianAtzori <brianatzori@gmail.com>
2023-02-21 14:54:06 +0100

    Fully functioning Dialog box with buttons

--- src/components/LoginOrTry.tsx
+++ src/components/LoginOrTry.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function LoginOrTry() {
   return (
     <div className="flex justify-center gap-5">
-      <button> Login </button>
-      <button> Prova Awaka </button>
+      <button className="bg-palette-color-darker p-2 w-1/4 rounded-md font-lato text-[#ffff]"> Login </button>
+      <button className="bg-palette-color-darker p-2 w-1/4 rounded-md font-lato text-[#ffff] animate-bounce"> Try Awaca </button>
     </div>
   );
 }
