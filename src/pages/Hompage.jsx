d67229faf27f7f10050aff6fbbf14e242908a33b
BrianAtzori <brianatzori@gmail.com>
2023-03-09 11:26:35 +0100

    Cleaning Dialog Component and code after SAL

--- src/pages/Hompage.jsx
+++ src/pages/Hompage.jsx
@@ -1,23 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
-import Dialog from "../components/Dialog";
 import MeditationSection from "../components/MeditationSection";
 import Hero from "../components/Hero";
 
 //A simple flag in the state will determine if the dialog is needed and if needed in which version
 
 export default function Hompage() {
-  const showDialog = useSelector((state) => state.dialog.value);
   return (
     <>
-      {showDialog && <Dialog></Dialog>}
-      {showDialog || (
-        <>
           <Hero></Hero>
           <MeditationSection></MeditationSection>
-        </>
-      )}
     </>
   );
 }
